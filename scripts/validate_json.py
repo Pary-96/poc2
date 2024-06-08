1672575a3c2f67577e4be3e7967159295cecc868
Pary-96 <parisrahmanss@gmail.com>
2024-06-08 21:56:19 +0530

    Adding actions

---
+++ scripts/validate_json.py
@@ -0,0 +1,25 @@
+import json
+import sys
+
+
+def check_json(file_path):
+    try:
+        with open(file_path) as f:
+            json.load(f)
+    except json.JSONDecodeError as e:
+        print(f"Error in file {file_path}: {e}")
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python validate_json.py <file_path>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    if check_json(file_path):
+        print(f"File {file_path} is a valid JSON file.")
+        sys.exit(0)
+    else:
+        print(f"File {file_path} is not a valid JSON file.")
+        sys.exit(1)
